43a9cac5895f0698ccff7cd69bd6aa1117c3ff18
AdwayB <adwaybyju@gmail.com>
2024-06-26 16:08:21 +0530

    db indexes updated

--- Helpers/DatabaseContext.cs
+++ Helpers/DatabaseContext.cs
@@ -7,4 +7,11 @@ public class DatabaseContext : DbContext {
   public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
   public DbSet<User> Users { get; set; }
+  
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<User>()
+      .HasIndex(e => new { e.UserId, e.Username, e.Email });
+  }
+
 }
